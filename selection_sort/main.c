3f028a1dac067e2b4ab8e59b3bef515435282a53
LoneDespairYt <lonedespairyt@gmail.com>
2023-04-05 20:15:07 +0800

    created combine for merge_sort

--- selection_sort/main.c
+++ selection_sort/main.c
@@ -12,7 +12,8 @@ int main()
     int input_list[] = {4, 1, 454, 234, 232, 11, 2};
     int size = sizeof(input_list) / sizeof(int);
 
-    selection_sort(size, input_list);
+    //selection_sort(size, input_list);
+    merge_sort()
     print_array(size, input_list);
 
     return 0;
@@ -40,11 +41,7 @@ void selection_sort(int size, int input_list[])
     }
 
     printf("Array size %d\n", size);
-
 }
-
-
-
 
 
 void merge_sort(int start, int end, int input_list[])
@@ -60,14 +57,54 @@ void merge_sort(int start, int end, int input_list[])
 }
 
 
+void combine(int start, int end, int input_list[])
+{
+    int middle = (end + start) / 2, size = end - start;
+    int temp_list[size];
+
+    // Copy left and right chunks to the temporary array
+    for (int idx = start; idx < end; idx++)
+        temp_list[idx - start] = input_list[idx];
+
+
+    int left = 0, right = middle, idx = start;
+
+    while (left < middle && right < size)
+    {
+        if (temp_list[left] <= temp_list[right])
+        {
+            input_list[idx] = temp_list[left];
+            left++;
+        }
+        else
+        {
+            input_list[idx] = temp_list[right];
+            right++;
+        }
+        idx++;
+    }
+
+    while(left < middle)
+    {
+        input_list[idx] = temp_list[left];
+        left++;
+        idx++;
+    }
+
+    while (right < size)
+    {
+        input_list[idx] = temp_list[left];
+        right++;
+        idx++;
+    }
+
+}
 
 
 void print_array(int size, int input_list[])
 {
     for (int first = 0; first < size; first++)
-    {
         printf("%d ", input_list[first]);
-    }
 
     printf("\n");
 }
