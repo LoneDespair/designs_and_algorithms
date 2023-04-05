0f9f99fae051566e6f38ad967a7bbb2b37861dd6
LoneDespairYt <lonedespairyt@gmail.com>
2023-04-05 11:24:09 +0800

    added print_array

--- selection_sort/main.c
+++ selection_sort/main.c
@@ -8,12 +8,11 @@ void print_array(int size, int input_list[]);
 int main()
 {
     int input_list[] = {4, 1, 454, 234, 232, 11, 2};
-
     int size = sizeof(input_list) / sizeof(int);
 
     selection_sort(size, input_list);
+    print_array(size, input_list);
 
-    printf("Main size %d\n", size);
     return 0;
 }
 
@@ -21,16 +20,16 @@ int main()
 void selection_sort(int size, int input_list[])
 {
 
-    for (first = 0; first < size; first++)
+    for (int first = 0; first < size; first++)
     {
         int min_value = input_list[first];
 
-        for (second = first + 1; second < size; second++)
+        for (int second = first + 1; second < size; second++)
         {
 
             if (input_list[second] < min_value)
             {
-                min_value = input[second];
+                min_value = input_list[second];
 
                 input_list[second] = input_list[first];
                 input_list[first] = min_value;
@@ -45,7 +44,12 @@ void selection_sort(int size, int input_list[])
 
 void print_array(int size, int input_list[])
 {
+    for (int first = 0; first < size; first++)
+    {
+        printf("%d ", input_list[first]);
+    }
 
+    printf("\n");
 }
 
 
